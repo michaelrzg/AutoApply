4816dbc12d8a8a12add831ce80144ef82c544251
michaelrzg <michaelrizig@gmail.com>
2024-09-28 20:15:51 -0400

    added drag to drop easter egg, added upload button functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,37 @@
-var uploadButton = document.getElementById("UploadResumeButton");
-var home = document.getElementById("brute");
-var uploadPage = document.getElementById("upload");
-uploadPage.style.display = "none";
+// main driver for AutoApply Extension
 
-uploadButton.onclick = function () {
-  document.getElementById("upload").style.display = "block";
-  document.getElementById("brute").style.display = "none";
-  initUploader();
-};
-
+// initilize some elements to variables for easier access and add
+function init() {
+  uploadButton = document.getElementById("UploadResumeButton");
+  home = document.getElementById("brute");
+  uploadPage = document.getElementById("upload");
+  uploadPage.style.display = "none";
+  // set onClick functionality for our upload button:
+  uploadButton.onclick = function () {
+    // make upload tile visible
+    document.getElementById("upload").style.display = "block";
+    document.getElementById("brute").style.display = "none";
+    // initilize file uploader code
+    initUploader();
+  };
+}
+// this function sets up the uploader
 function initUploader() {
+  // init drop area and manual uplpad button
   const droparea = document.getElementById("header");
   const uploadManually = document.getElementById("file");
+  const beforestate = droparea.innerHTML;
+  // DEFINE states for drop area
+  // active = a file is currently hovering above the drop zone
   const active = () => {
-    droparea.style.borderColor = "#00ff00";
+    document.getElementById("cloudsvg").style.display = "none";
+    document.getElementById("feedme").style.display = "block";
+    document.getElementById("headerptext").innerHTML = "FEED ME";
   };
+  // a file is hovering on screen, but is not over teh drop zone
   const inactive = () => {
     droparea.style.borderColor = "black";
+    droparea.innerHTML = beforestate;
   };
   const prevents = (e) => e.preventDefault();
 
@@ -31,18 +46,37 @@ function initUploader() {
     droparea.addEventListener(eventName, inactive);
   });
   droparea.addEventListener("drop", handleDrop);
+
   uploadManually.addEventListener("change", (e) => {
     const file = e.target.files;
     console.log(file);
+    resume = file;
+    // placeholder:
+    uploadPage.style.display = "none";
+    home.style.display = "block";
   });
 }
-[];
 const handleDrop = (e) => {
   const data = e.dataTransfer;
   const file = data.files;
   console.log(file);
+  resume = file;
 
   // placeholder:
   uploadPage.style.display = "none";
   home.style.display = "block";
 };
+
+// MAIN:
+
+// variable holds the resume file
+var resume;
+// upload button on home page
+var uploadButton;
+// home page tile
+var home;
+// upload page tile
+var uploadPage;
+
+// initilize our variables
+init();
